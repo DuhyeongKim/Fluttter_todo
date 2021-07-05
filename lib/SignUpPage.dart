ec2c590b8d7789b4681e926099cfc04ea399147e
DuhyeongKim <21400080@handong.edu>
2021-07-05 16:30:59 +0900

    second login

--- lib/SignUpPage.dart
+++ lib/SignUpPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 
 class SingUpPage extends StatefulWidget {
@@ -21,7 +22,38 @@ class _SingUpPageState extends State<SingUpPage> {
         child: Column(
            mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Sing Up"),
+            Text("Sing Up",
+            style: TextStyle(
+              fontSize: 35,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width-60,
+              height: 60,
+              child: Card(
+                color: Colors.black,
+                elevation: 8,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25),
+                    side: BorderSide(
+                      width: 1, color: Colors.grey,
+                    ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SvgPicture.asset("assets/google.svg", height:25, width: 25,),
+                    SizedBox(width: 15,),
+                    Text("Continue with Google"),
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
